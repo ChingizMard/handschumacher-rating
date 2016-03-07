bdf8bad9f8987bfc6446f472f699d86a7da461fa
Chingiz Mardanov <chingiz@li.ru>
2016-03-06 22:36:29 -0500

    Parsing the menu and stations

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,15 +1,7 @@
 angular.module('starter.controllers', ['ui.bootstrap','ng'])
 
-.controller('RateCtrl', function($scope) {
+.controller('RateCtrl', function($scope, $rootScope) {
   
-  $scope.stations = [{name: 'Grill', description: 'Burger'},
-                      {name: 'Grill2', description: 'Burger'},
-                      {name: 'Grill3', description: 'Burger'},
-                      {name: 'Grill4', description: 'Burger'},
-                      {name: 'Grill5', description: 'Burger'},
-                      {name: 'Grill6', description: 'Burger'},
-                      {name: 'Grill7', description: 'Burger'},
-                      {name: 'Grill8', description: 'Burger'}];
 
 })
 .controller('StatsCtrl', function($scope) {
@@ -20,45 +12,105 @@ angular.module('starter.controllers', ['ui.bootstrap','ng'])
 
 
 
-.controller('AccountCtrl', function($scope) {
-  $scope.settings = {
-    enableFriends: true
-  };
-})
+// .controller('AccountCtrl', function($scope) {
+//   $scope.settings = {
+//     enableFriends: true
+//   };
+// })
 
 
-.controller('DashCtrl', function($scope, $http){
-  scrollToLoc('firstCard');
+.controller('DashCtrl', function($scope, $http, $rootScope){
+  
+  
+
 
   $scope.averageRate = 3.2; //To be calculated on the BackEnd
   $scope.rate = Math.round($scope.averageRate);
   $scope.oneOpenOnly = true;
-
+  $scope.stations = new Array();
   //Replace that array, by pooling from server.
-  $scope.stations = [{name: 'Grill', description: 'Burger'},
-                      {name: 'Grill', description: 'Burger'},
-                      {name: 'Grill', description: 'Burger'},
-                      {name: 'Grill', description: 'Burger'},
-                      {name: 'Grill', description: 'Burger'},
-                      {name: 'Grill', description: 'Burger'},
-                      {name: 'Grill', description: 'Burger'},
-                      {name: 'Grill', description: 'Burger'}];
+  // $scope.stations = [{name: 'Grill', description: 'Burger'},
+  //                     {name: 'Grill', description: 'Burger'},
+  //                     {name: 'Grill', description: 'Burger'},
+  //                     {name: 'Grill', description: 'Burger'},
+  //                     {name: 'Grill', description: 'Burger'},
+  //                     {name: 'Grill', description: 'Burger'},
+  //                     {name: 'Grill', description: 'Burger'},
+  //                     {name: 'Grill', description: 'Burger'}];
 
 
-  function scrollToLoc(hash) {
-    location.hash = "#" + hash;
-  }        
+    
 
 
   $http({
     method: 'GET',
     url: 'http://tux64-14.cs.drexel.edu:8080/weeklymenu'
   }).then(function successCallback(response) {
-    console.log(response.data.days[1]);
-    //console.log(root.days[0].brk[0].station_name);
+    console.log(response.data);
+    var date = new Date();
+    var currentHour = date.getHours();
+    var daysOfTheWeek = ['monday', 'tuesday', 'wednesday', 'thursday', 'friday'];
+    var currentDate = daysOfTheWeek[date.getDay()];
+
+    currentDate = 'monday';
+
+    if (currentHour > 0 && currentHour < 12){
+    //for(var item in JSONResponse.)
+
+    }else if(currentHour > 12 && currentHour < 16 ){
+    //Do lunch
+
+    }else{
+      //Do dinner 
+
+      switch(currentDate) {
+          case 'monday':
+              for (var item in response.data.days[0].monday.din){
+                var dishes = response.data.days[0].monday.din[item].items;
+                var stationName = response.data.days[0].monday.din[item].station_name;
+                $scope.stations.push({name: stationName, description:dishes});
+              }
+              break;
+          case 'tuesday':
+              for (var item in response.data.days[1].tuesday.din){
+                $scope.stations.push(response.data.days[1].tuesday.din[item].station_name);
+              }
+              break;
+          case 'wednesday':
+              for (var item in response.data.days[2].wednesday.din){
+                $scope.stations.push(response.data.days[2].wednesday.din[item].station_name);
+              }
+              break;
+          case 'thursday':
+             for (var item in response.data.days[3].thursday.din){
+                $scope.stations.push(response.data.days[3].thursday.din[item].station_name);
+              }
+              break;
+          case 'friday':
+             for (var item in response.data.days[4].friday.din){
+                $scope.stations.push(response.data.days[4].friday.din[item].station_name);
+              }
+
+              break;    
+          default:       
+      }
+
+      $rootScope.stations = $scope.stations;
+
+    }
+    
   }, function errorCallback(response) {
+
     // called asynchronously if an error occurs
     // or server returns response with an error status.
   });
+
+  //Populating stations by time
+  var date = new Date();
+  var currentHour = date.getHours();
+  var daysOfTheWeek = ['monday', 'tuesday', 'wednesday', 'thursday', 'friday'];
+  
+
+
 
 });
